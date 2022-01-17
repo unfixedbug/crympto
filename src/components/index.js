800a88b36b467c7bb3c435324f99dfed36132274
Dhiraj <jdhiraj543@gmail.com>
2022-01-17 16:15:27 +0530

    components added

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+
+// common file to clear clutter to export from a single file
+export { default as Welcome } from "./Welcome";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Loader } from "./Loader";
+export { default as Transactions } from "./Transactions";
+export { default as Services } from "./Services";
