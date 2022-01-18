0a0c2e2b704d7a48c5ea0f12c7aeba4f415644a2
Dhiraj <jdhiraj543@gmail.com>
2022-01-18 10:43:55 +0530

    etherium cards frontend

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Loader = () => {
-  return <div></div>;
+  return <div className="flex justify-center items-center py-3">
+    <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700"/>
+  </div>;
 };
 
 export default Loader;
