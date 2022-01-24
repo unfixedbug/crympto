5fabf1d6dace7893bb740981cde747e69644117e
Dhiraj <jdhiraj543@gmail.com>
2022-01-24 11:30:34 +0530

    custom footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,15 +17,15 @@ const Footer = () => (
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
-      <p className="text-white text-sm text-center font-medium mt-2">info@kryptomastery.com</p>
+      <p className="text-white text-sm text-center">Did you like it ?  </p>
+      <p className="text-white text-sm text-center font-medium mt-2"><a href="www.dhirajjadhav.me">unfixedbug</a></p>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">@kryptomastery2022</p>
-      <p className="text-white text-right text-xs">All rights reserved</p>
+      <p className="text-white text-left text-xs"><a href="www.dhirajjadhav.me" >@unfixedbug</a></p>
+      <p className="text-white text-right text-xs">web3 is fun</p>
     </div>
   </div>
 );
