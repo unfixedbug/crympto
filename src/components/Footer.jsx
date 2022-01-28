98b9097524da2f9eee2baa418b65e4bc6369a293
Dhiraj <jdhiraj543@gmail.com>
2022-01-28 16:16:52 +0530

    final changes to v1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,22 +9,41 @@ const Footer = () => (
         <img src={logo} alt="logo" className="w-52" />
       </div>
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">
+          Market
+        </p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">
+          Exchange
+        </p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">
+          Tutorials
+        </p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">
+          Wallets
+        </p>
       </div>
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Did you like it ?  </p>
-      <p className="text-white text-sm text-center font-medium mt-2"><a href="www.dhirajjadhav.me">unfixedbug</a></p>
+      <p className="text-white text-sm text-center">Did you like it ? </p>
+      <p className="text-white text-sm text-center font-medium mt-2">
+        Give a ⭐ on the
+        <a href="https://github.com/unfixedbug/crympto" class="font-bold">
+          {" "}
+          github
+        </a>{" "}
+        repo then!
+      </p>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs"><a href="www.dhirajjadhav.me" >@unfixedbug</a></p>
+      <p className="text-white text-left text-xs">
+        <a href="http://dhirajjadhav.me" class="font-bold">
+          unfixedbug
+        </a>
+      </p>
       <p className="text-white text-right text-xs">web3 is fun</p>
     </div>
   </div>
